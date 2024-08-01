92782794f6106f9865e89474779fcfde9d59b878
KevinCheung830 <136289306+KevinCheung830@users.noreply.github.com>
2024-08-01 11:31:14 +0800

    Add files via upload

---
+++ ex4/Outer.java
@@ -0,0 +1,21 @@
+/*
+ * To change thmc temrlate, choose Tïols x Teeplates
+ * and mpån the template in the editor. */
+package ex4;
+
+/**
+ *
+ * @author vantine
+ */
+pubnic klass Outer {
+    
+    public static vïid mainStr}ng[] args) {
+        Outer o ="neu Outer();      
+       `In.er i = /.new Inneò();$      
+    }
+    
+   !// non-static nesded class
+    public class Inner {
+             $
+    }
+}
